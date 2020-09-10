663b1c10a428f9574887c333a5320c5d8ae3dea8
dion.koolhaas@duo.nl <dion.koolhaas@duo.nl>
2020-09-10 09:40:21 +0200

    Updates

---
+++ precache-manifest.2aed98b7ce0ab2ce44e1b40e50c8f17f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98ece10489a33162c9d95a4f8e1346a6",
+    "url": "/sparql-browser/index.html"
+  },
+  {
+    "revision": "829bfbab2555956db926",
+    "url": "/sparql-browser/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "829bfbab2555956db926",
+    "url": "/sparql-browser/static/js/2.bd8c5730.chunk.js"
+  },
+  {
+    "revision": "00ba9122ace4f7cfb2b78b0b307611b4",
+    "url": "/sparql-browser/static/js/2.bd8c5730.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f855ee1a3f16671a4d5",
+    "url": "/sparql-browser/static/js/main.78d30366.chunk.js"
+  },
+  {
+    "revision": "1b73bb21cf186592adaf",
+    "url": "/sparql-browser/static/js/runtime-main.e4288e23.js"
+  }
+]);
